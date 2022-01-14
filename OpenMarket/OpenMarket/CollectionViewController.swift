c55c06ee293f9637620634e131cf1f8714a25412
hoBahk <qudgh849@gmail.com>
2022-01-14 09:06:13 +0900

    refactor: 레이아웃 방식 변경

--- OpenMarket/OpenMarket/CollectionViewController.swift
+++ OpenMarket/OpenMarket/CollectionViewController.swift
@@ -44,10 +44,11 @@ class CollectionViewController: UIViewController {
   private func changeCollectionViewLayout(by selectedSegmentIndex: Int) {
     switch selectedSegmentIndex {
     case 0:
-      collectionView.collectionViewLayout = createListViewLayout()
+      //collectionView.collectionViewLayout = createListViewLayout()
+      //self.collectionView.delegate = self
       configureListViewDataSource()
     case 1:
-      collectionView.collectionViewLayout = createGridViewLayout()
+      //collectionView.collectionViewLayout = createGridViewLayout()
       configureGridViewDataSource()
     default:
       return
@@ -74,7 +75,7 @@ class CollectionViewController: UIViewController {
       case .success(let data):
         self.product = data.pages
         DispatchQueue.main.async {
-          self.collectionView.collectionViewLayout = self.createListViewLayout()
+          self.collectionView.delegate = self
           self.configureListViewDataSource()
           self.activityIndicator.removeFromSuperview()
         }
@@ -93,46 +94,76 @@ class CollectionViewController: UIViewController {
   }
 }
 
-extension CollectionViewController {
-  private func createListViewLayout() -> UICollectionViewLayout {
-    let config = UICollectionLayoutListConfiguration(appearance: .plain)
-    return UICollectionViewCompositionalLayout.list(using: config)
-  }
-  
-  private func createGridViewLayout() -> UICollectionViewLayout {
-    let layout = UICollectionViewCompositionalLayout{
-      // 만들게 되면 튜플 (키: 값, 키: 값) 의 묶음으로 들어옴 반환 하는 것은 NSCollectionLayoutSection 콜렉션 레이아웃 섹션을 반환해야함
-      (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-      
-      // 아이템에 대한 사이즈 - absolute 는 고정값, estimated 는 추측, fraction 퍼센트
-      let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-      
-      // 위에서 만든 아이템 사이즈로 아이템 만들기
-      let item = NSCollectionLayoutItem(layoutSize: itemSize)
-      
-      // 아이템 간의 간격 설정
-      item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)
-      
-      // 변경할 부분
-      let groupHeight =  NSCollectionLayoutDimension.fractionalWidth(0.677)
-      
-      // 그룹사이즈
-      let grouSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
-      
-      // 변경할 부분
-      let group = NSCollectionLayoutGroup.horizontal(layoutSize: grouSize, subitem: item, count: 2)
-      
-      // 그룹으로 섹션 만들기
-      let section = NSCollectionLayoutSection(group: group)
-      //            section.orthogonalScrollingBehavior = .groupPaging
-      
-      // 섹션에 대한 간격 설정
-      section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
-      return section
-    }
-    return layout
-  }
-}
+//extension CollectionViewController {
+//  private func createListViewLayout() -> UICollectionViewLayout {
+//    let layout = UICollectionViewCompositionalLayout{
+//      // 만들게 되면 튜플 (키: 값, 키: 값) 의 묶음으로 들어옴 반환 하는 것은 NSCollectionLayoutSection 콜렉션 레이아웃 섹션을 반환해야함
+//      (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+//
+//      // 아이템에 대한 사이즈 - absolute 는 고정값, estimated 는 추측, fraction 퍼센트
+//      let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+//
+//      // 위에서 만든 아이템 사이즈로 아이템 만들기
+//      let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//
+//      // 아이템 간의 간격 설정
+//      //item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+//
+//      // 변경할 부분
+//      let groupHeight = NSCollectionLayoutDimension.fractionalHeight(0.083)
+//
+//      // 그룹사이즈
+//      let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+//
+//      // 변경할 부분
+//      let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+//
+//      // 그룹으로 섹션 만들기
+//      let section = NSCollectionLayoutSection(group: group)
+//      //            section.orthogonalScrollingBehavior = .groupPaging
+//
+//      // 섹션에 대한 간격 설정
+////      section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 10, trailing: 0)
+//      return section
+//    }
+//    return layout
+//  }
+//  }
+//
+//  private func createGridViewLayout() -> UICollectionViewLayout {
+//    let layout = UICollectionViewCompositionalLayout{
+//      // 만들게 되면 튜플 (키: 값, 키: 값) 의 묶음으로 들어옴 반환 하는 것은 NSCollectionLayoutSection 콜렉션 레이아웃 섹션을 반환해야함
+//      (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+//
+//      // 아이템에 대한 사이즈 - absolute 는 고정값, estimated 는 추측, fraction 퍼센트
+//      let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+//
+//      // 위에서 만든 아이템 사이즈로 아이템 만들기
+//      let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//
+//      // 아이템 간의 간격 설정
+//      item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10)
+//
+//      // 변경할 부분
+//      let groupHeight = NSCollectionLayoutDimension.fractionalWidth(0.677)
+//
+//      // 그룹사이즈
+//      let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+//
+//      // 변경할 부분
+//      let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+//
+//      // 그룹으로 섹션 만들기
+//      let section = NSCollectionLayoutSection(group: group)
+//      //            section.orthogonalScrollingBehavior = .groupPaging
+//
+//      // 섹션에 대한 간격 설정
+//      section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+//      return section
+//    }
+//    return layout
+//  }
+
 
 extension CollectionViewController {
   private func configureListViewDataSource() {
@@ -186,4 +217,44 @@ extension CollectionViewController {
 
 extension CollectionViewController: UICollectionViewDelegate {
   
+}
+
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let width = collectionView.frame.size.width
+    if segmentControl.selectedSegmentIndex == 0 {
+      return CGSize(width: width, height: width / 7)
+    } else {
+      return CGSize(width: width / 2.2, height: width / 1.5)
+    }
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                     layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+    if segmentControl.selectedSegmentIndex == 0 {
+      return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    } else {
+      return UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8)
+    }
+  }
+  
+
+  func collectionView(_ collectionView: UICollectionView,
+                     layout collectionViewLayout: UICollectionViewLayout,
+                      minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    if segmentControl.selectedSegmentIndex == 0 {
+      return 5
+    } else {
+      return 8
+    }
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                     layout collectionViewLayout: UICollectionViewLayout,
+                      minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    return 5
+  }
 }
